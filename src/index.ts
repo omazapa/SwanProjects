2543eaf9884365b65c7194c0408b78b98222fd79
Omar Zapata <Omar.Zapata@cern.ch>
2021-05-10 17:41:45 +0200

    appliying more changes for the migration to lab3

--- src/index.ts
+++ src/index.ts
@@ -7,26 +7,26 @@ import {
 } from '@jupyterlab/application';
 
 import { ICommandPalette } from '@jupyterlab/apputils';
-// import { swanProjectIcon } from './icons';
+import { swanProjectIcon } from './icons';
 
 import { IFileBrowserFactory } from '@jupyterlab/filebrowser';
 
-// const PALETTE_CATEGORY = 'Project';
+const PALETTE_CATEGORY = 'Project';
 
-// import { ProjectDialog } from './ProjectDialog';
+import { ProjectDialog } from './ProjectDialog';
 
 /**
  * The command IDs used by the server extension plugin.
  */
-// namespace CommandIDs {
-//   export const projectDialog = 'swan:create-project-dialog';
-//   export const projectDialogEdit = 'swan:edit-project-dialog';
-// }
+namespace CommandIDs {
+  export const projectDialog = 'swan:create-project-dialog';
+  export const projectDialogEdit = 'swan:edit-project-dialog';
+}
 
 import { ILauncher } from '@jupyterlab/launcher';
-// import { kernelsInfoRequest } from './request';
+import { kernelsInfoRequest } from './request';
 
-//export { request };
+// export { request };
 
 /**
  * Initialization data for the server-extension-example extension.
@@ -44,68 +44,68 @@ const extension: JupyterFrontEndPlugin<void> = {
     browserFactory: IFileBrowserFactory
   ) => {
     console.log('JupyterLab extension swanprojects is activated!');
-    // const { commands } = app;
-    // commands.addCommand(CommandIDs.projectDialog, {
-    //   icon: swanProjectIcon,
-    //   label: 'New',
-    //   caption: 'New',
-    //   execute: async args => {
-    //     const stacks = await kernelsInfoRequest();
-    //     ProjectDialog.OpenModal(
-    //       {
-    //         name: '',
-    //         stack: '',
-    //         release: '',
-    //         platform: '',
-    //         user_script: '', // eslint-disable-line @typescript-eslint/camelcase
-    //         stacks_options: stacks['stacks'] // eslint-disable-line @typescript-eslint/camelcase
-    //       },
-    //       true,
-    //       commands
-    //     );
-    //   }
-    // });
+    const { commands } = app;
+    commands.addCommand(CommandIDs.projectDialog, {
+      icon: swanProjectIcon,
+      label: 'New',
+      caption: 'New',
+      execute: async args => {
+        const stacks = await kernelsInfoRequest();
+        ProjectDialog.OpenModal(
+          {
+            name: '',
+            stack: '',
+            release: '',
+            platform: '',
+            user_script: '', // eslint-disable-line @typescript-eslint/camelcase
+            stacks_options: stacks['stacks'] // eslint-disable-line @typescript-eslint/camelcase
+          },
+          true,
+          commands
+        );
+      }
+    });
 
-    // commands.addCommand(CommandIDs.projectDialogEdit, {
-    //   icon: swanProjectIcon,
-    //   label: 'Edit',
-    //   caption: 'Edit',
-    //   execute: async args => {
-    //     const stacks = await kernelsInfoRequest();
-    //     const result = await ProjectDialog.OpenModal(
-    //       {
-    //         name: args.name as string,
-    //         stack: args.stack as string,
-    //         release: args.release as string,
-    //         platform: args.platform as string,
-    //         user_script: args.user_script as string, // eslint-disable-line @typescript-eslint/camelcase
-    //         stacks_options: stacks['stacks'] // eslint-disable-line @typescript-eslint/camelcase
-    //       },
-    //       false,
-    //       commands
-    //     );
-    //     return result;
-    //   }
-    // });
+    commands.addCommand(CommandIDs.projectDialogEdit, {
+      icon: swanProjectIcon,
+      label: 'Edit',
+      caption: 'Edit',
+      execute: async args => {
+        const stacks = await kernelsInfoRequest();
+        const result = await ProjectDialog.OpenModal(
+          {
+            name: args.name as string,
+            stack: args.stack as string,
+            release: args.release as string,
+            platform: args.platform as string,
+            user_script: args.user_script as string, // eslint-disable-line @typescript-eslint/camelcase
+            stacks_options: stacks['stacks'] // eslint-disable-line @typescript-eslint/camelcase
+          },
+          false,
+          commands
+        );
+        return result;
+      }
+    });
 
     // // Add the command to the launcher
-    // if (launcher) {
-    //   launcher.add({
-    //     command: CommandIDs.projectDialog,
-    //     category: PALETTE_CATEGORY,
-    //     rank: 1,
-    //     kernelIconUrl: ''
-    //   });
-    // }
+    if (launcher) {
+      launcher.add({
+        command: CommandIDs.projectDialog,
+        category: PALETTE_CATEGORY,
+        rank: 1,
+        kernelIconUrl: ''
+      });
+    }
 
     // // Add the command to the palette
-    // if (palette) {
-    //   palette.addItem({
-    //     command: CommandIDs.projectDialog,
-    //     args: { isPalette: true },
-    //     category: PALETTE_CATEGORY
-    //   });
-    // }
+    if (palette) {
+      palette.addItem({
+        command: CommandIDs.projectDialog,
+        args: { isPalette: true },
+        category: PALETTE_CATEGORY
+      });
+    }
   }
 };
 
