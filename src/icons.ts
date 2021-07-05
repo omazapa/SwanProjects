ca37f385fa79ba3a43b0c81a5b0eb74d09503e37
Omar Zapata <Omar.Zapata@cern.ch>
2021-07-05 17:07:58 +0200

    removed unneeded icons

--- src/icons.ts
+++ src/icons.ts
@@ -1,14 +1,8 @@
 import { LabIcon } from '@jupyterlab/ui-components';
 
 import swanProjectIconStr from '../style/project.svg';
-import swanProjectImportIconStr from '../style/cloud-download-alt.svg';
 import swanReadmeIconStr from '../style/list-alt.svg';
-import swanConfigIconStr from '../style/cog.svg';
-import swanProjectsIconStr from '../style/project-diagram.svg';
-
-import cernboxIconStr from '../style/cernbox.svg';
 import cmsIconStr from '../style/cms.svg';
-import condaIconStr from '../style/anaconda_logo_circle.svg';
 import sftIconStr from '../style/sft.svg';
 
 export const swanProjectIcon = new LabIcon({
@@ -16,39 +10,14 @@ export const swanProjectIcon = new LabIcon({
   svgstr: swanProjectIconStr
 });
 
-export const swanProjectImportIcon = new LabIcon({
-  name: 'jupyterlab_swan:project_import',
-  svgstr: swanProjectImportIconStr
-});
-
 export const swanReadmeIcon = new LabIcon({
   name: 'jupyterlab_swan:reame',
   svgstr: swanReadmeIconStr
 });
 
-export const swanConfigIcon = new LabIcon({
-  name: 'jupyterlab_swan:config',
-  svgstr: swanConfigIconStr
-});
-
-export const swanProjectsIcon = new LabIcon({
-  name: 'jupyterlab_swan:projects',
-  svgstr: swanProjectsIconStr
-});
-
-export const cernboxIcon = new LabIcon({
-  name: 'jupyterlab_swan:cernbox',
-  svgstr: cernboxIconStr
-});
-
 export const cmsIcon = new LabIcon({
   name: 'jupyterlab_swan:cms',
   svgstr: cmsIconStr
-});
-
-export const condaIcon = new LabIcon({
-  name: 'jupyterlab_swan:conda',
-  svgstr: condaIconStr
 });
 
 export const sftIcon = new LabIcon({
